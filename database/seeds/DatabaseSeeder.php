295107d3d66df36eaa17cf9884c74a6917300186
MRIkaito <pnfb21dp@s.okayama-u.ac.jp>
2022-03-26 21:09:04 +0900

    データ削除， シーディングを一括化

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,52 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
+    public function run(){
         $this->call(PostsTableSeeder::class);
+    
+        //記事のシーディング　▶　なしにする
+        // $param = [
+        //     'title' => 'X線検査',
+        //     'contents' => '妊婦は気をつけよう',
+        //     'category_id' => 1,
+        //     'created_at'=>now(),
+        //     'updated_at'=>now(),
+        // ];
+        // DB::table('posts')->insert($param);
+    
+        // $param = [
+        //     'title' => 'CT検査',
+        //     'contents' => '被ばくは多い',
+        //     'category_id' => 2,
+        //     'created_at'=>now(),
+        //     'updated_at'=>now(),
+        // ];
+        // DB::table('posts')->insert($param);
+        
+        //カテゴリのシーディング
+        DB::table('categories')->insert([
+        'id'=>'1',
+        'name'=>'一般撮影'
+        ]);
+
+        DB::table('categories')->insert([
+        'id'=>'2',
+        'name'=>'CT'
+        ]);
+        
+        DB::table('categories')->insert([
+        'id'=>'3',
+        'name'=>'MRI'
+        ]);
+        
+        DB::table('categories')->insert([
+        'id'=>'4',
+        'name'=>'放射線治療'
+        ]);
+        
+        DB::table('categories')->insert([
+        'id'=>'5',
+        'name'=>'核医学'
+        ]);
     }
 }
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,24 +12,25 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        $param = [
-            'title' => 'X線検査',
-            'contents' => '妊婦は気をつけよう',
-            'category_id' => 1,
-            // 'user_id' => 1,
-            'created_at'=>now(),
-            'updated_at'=>now(),
-        ];
-        DB::table('posts')->insert($param);
+        //記事のシーディングはなし．
+        // $param = [
+        //     'title' => 'X線検査',
+        //     'contents' => '妊婦は気をつけよう',
+        //     'category_id' => 1,
+        //     // 'user_id' => 1,
+        //     'created_at'=>now(),
+        //     'updated_at'=>now(),
+        // ];
+        // DB::table('posts')->insert($param);
     
-        $param = [
-            'title' => 'CT検査',
-            'contents' => '被ばくは多い',
-            'category_id' => 2,
-            // 'user_id' => 2,
-            'created_at'=>now(),
-            'updated_at'=>now(),
-        ];
-        DB::table('posts')->insert($param);
+        // $param = [
+        //     'title' => 'CT検査',
+        //     'contents' => '被ばくは多い',
+        //     'category_id' => 2,
+        //     // 'user_id' => 2,
+        //     'created_at'=>now(),
+        //     'updated_at'=>now(),
+        // ];
+        // DB::table('posts')->insert($param);
     }
 }
